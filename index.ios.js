af1774e37f8a59e96db7199b95c6d28e1f9b04d0
okmttdhr <10101114fin@gmail.com>
2015-05-16 19:58:54 +0900

    listview

--- index.ios.js
+++ index.ios.js
@@ -15,6 +15,7 @@ var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/maste
 var {
   AppRegistry,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
@@ -31,7 +32,10 @@ var MOCKED_MOVIES_DATA = [
 var AwesomeProject = React.createClass({
   getInitialState: function() {
     return {
-      movies: null
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
     };
   },
   componentDidMount: function() {
@@ -42,18 +46,24 @@ var AwesomeProject = React.createClass({
       .then((response) => response.json())
       .then((responseData) => {
         this.setState({
-          movies: responseData.movies,
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
         });
       })
       .done();
   },
   render: function() {
-    if (!this.state.movies) {
+    if (!this.state.loaded) {
       return this.renderLoadingView();
     }
 
-    var movie = this.state.movies[0];
-    return this.renderMovie(movie);
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    );
   },
   renderLoadingView: function() {
     return (
@@ -102,6 +112,10 @@ var styles = StyleSheet.create({
   },
   year: {
     textAlign: 'center',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
